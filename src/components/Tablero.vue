3ef6ca0dfe6e2a148a0b334ac358a865ef134d28
hector.frasson <hector.frasson@lemondata.com.ar>
2023-07-03 04:02:46 -0300

    imagenes

--- src/components/Tablero.vue
+++ src/components/Tablero.vue
@@ -3,10 +3,19 @@
     <!-- Este es el componente vista que muestra todos los componentes que usan useApi -->
     <h1>Componente vista de tablero</h1>
     <!-- Estos son los componentes que usan useApi -->
-    <Coche />
-    <Luces />
+    <div class="imagen">
+      <img v-bind:src="cocheImg" alt="Coche" />
+      <Coche />
+    </div>
+    <div class="imagen">
+      <img v-bind:src="lucesImg" alt="Luces" />
+      <Luces />
+    </div>
     <!-- <Usuario /> -->
-    <Motor />
+    <div class="imagen">
+      <img v-bind:src="motorImg" alt="Motor" />
+      <Motor />
+    </div>
   </div>
 </template>
 
@@ -17,6 +26,12 @@ import Luces from './Luces.vue'
 import Usuario from './Usuario.vue'
 import Motor from './Motor.vue'
 
+// Importamos las imágenes que queremos usar
+import cocheImg from '../assets/coche.png'
+import lucesImg from '../assets/luces.jpg'
+import motorImg from '../assets/motor.jpg'
+import tableroImg from '../assets/tablero.png'
+
 export default {
   name: 'Tablero',
   components: {
@@ -25,6 +40,15 @@ export default {
     Usuario,
     Motor,
   },
+  data() {
+    return {
+      // Asignamos las imágenes importadas a unas propiedades del objeto data
+      cocheImg,
+      lucesImg,
+      motorImg,
+      tableroImg
+    }
+  }
 }
 </script>
 
@@ -45,5 +69,16 @@ h1 {
 
 .tablero > * {
   border: 2px solid #cccccc;
+}
+
+.imagen {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.imagen > img {
+  width: 100px;
+  height: auto;
 }
 </style>
